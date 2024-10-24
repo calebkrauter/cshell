0093b6de7e6fe83eef08ea9daefea0d8b7069a79
Caleb Krauter <ghostMe66+github@proton.me>
2024-10-23 17:26:43 -0700

    Base Code.

--- MyShell.c
+++ MyShell.c
@@ -1,4 +1,46 @@
-#include<stdio.h>
-void main() {
-    printf("Hello World");
+***************************************************************************//**
+  @file         MyShell.c
+  @author       Juhua Hu
+
+*******************************************************************************/
+
+/**
+DONOT change the existing function definitions. You can add functions, if necessary.
+*/
+
+
+
+/**
+  @brief Fork a child to execute the command using execvp. The parent should wait for the child to terminate
+  @param args Null terminated list of arguments (including program).
+  @return returns 1, to continue execution and 0 to terminate the MyShell prompt.
+ */
+int execute(char **args)
+{
+
+}
+
+
+/**
+  @brief gets the input from the prompt and splits it into tokens. Prepares the arguments for execvp
+  @return returns char** args to be used by execvp
+ */
+char** parse(void)
+{
+
+}
+
+
+/**
+   @brief Main function should run infinitely until terminated manually using CTRL+C or typing in the exit command
+   It should call the parse() and execute() functions
+   @param argc Argument count.
+   @param argv Argument vector.
+   @return status code
+ */
+int main(int argc, char **argv)
+{
+
+
+  return EXIT_SUCCESS;
 }
