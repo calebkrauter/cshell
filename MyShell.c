92ddf6809f9ba25bc497b9173389934ce3964487
Caleb Krauter <ghostMe66+github@proton.me>
2024-10-23 19:56:12 -0700

    Partial implementation of main.

--- MyShell.c
+++ MyShell.c
@@ -1,8 +1,14 @@
-***************************************************************************//**
+#include <stdio.h>
+#include <string.h>
+/***************************************************************************
   @file         MyShell.c
   @author       Juhua Hu
+  @author       Caleb Krauter
 
 *******************************************************************************/
+
+
+
 
 /**
 DONOT change the existing function definitions. You can add functions, if necessary.
@@ -38,9 +44,36 @@ char** parse(void)
    @param argv Argument vector.
    @return status code
  */
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
+  int EXIT_SUCCESS = 1;
+  char input[250];
+  const char *exit1 = "exit";
+  const char cont = '\0';
+  // scanf("%s", &input);
 
+  int length = 0;
+  printf("MyShell> ");
+ while (1) {
+  char curChar = getchar();
+    printf("MyShell> ");
+    length++;
+    // printf("MyShell> ");
+if (curChar == '\n') {
+      continue; 
+    }
+    if (length > 1) {
+      scanf("%99s", &input);
+      continue;
+    } else if (length == 1) {
+      scanf("%c", &input);
+      continue; 
+    } 
+    
+    if (strcmp(input, exit1) == 0) {
+      EXIT_SUCCESS = 0;
+      break;
+    }
+  }
 
   return EXIT_SUCCESS;
 }
