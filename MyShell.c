42acd354316d4e5a1eecf1f1dc31a25ff14dfa48
Caleb Krauter <ghostMe66+github@proton.me>
2024-10-25 11:09:09 -0700

    Implement argument counter.

--- MyShell.c
+++ MyShell.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 /***************************************************************************
   @file         MyShell.c
   @author       Juhua Hu
@@ -49,27 +50,54 @@ int main(int argc, char **argv) {
   char input[250];
   const char *exit1 = "exit";
   const char cont = '\0';
+  int *argumentCount = 0;
+  const char *command = getenv("MY_ARG");
 
  while (1) {
   printf("MyShell> ");
-  // ChatGPT reccomended using getchar()
+  
+  // printf("%s", argv[0]);
+  // I needed a way to check the current character to account for newlines, ChatGPT suggested using getchar()
   char curChar = getchar();
   if (curChar == '\n') {
-      
-    continue; 
+    // argumentCount = 0;
+    
+    continue;
   } else {
-    // ChatGPT reccomended this function ungetc()
+    // I needed a way to reverse iteration of getchar() so ungetc() puts it back onto the stream 
+    // to ensure fgets() has the whole line of input, ChatGPT suggested this function ungetc()
     ungetc(curChar, stdin);
   }
-  // ChatGPT reccomended this function fgets()
+  // I needed a way to take user input that wasn't constrained to a specfic type of data. Scanf() with %s would not include single characters
+  // and %c would not include full strings, ChatGPT suggested this function fgets()
   fgets(input, sizeof(input), stdin);
+  
+  countArguments(&input, &argumentCount);
   // printf("MyShell> %s=%s\n", input, exit1);
-  // ChatGPT reccomended using strcmp()
+  if (strstr(input, ""))
+  // I needed a way to compare two strings and didn't want to manually compare them, ChatGPT suggested using strcmp()
     if (strcmp(input, "exit\n") == 0) {
       EXIT_SUCCESS = 0;
       break;
     }
+      // printf("%d", argumentCount);
   }
 
   return EXIT_SUCCESS;
+}
+
+// 2 Used chatGPT to determine how to use argumentCount correctly as a pointer.
+void countArguments(char *input, int *argumentCount) {
+  *argumentCount = 0;
+  for (int n = 0; n < strlen(input); n++) {
+    // printf("%d", n);
+    if (!isspace(input[0]) && n == 0) {
+      *argumentCount = *argumentCount + 1;
+      
+    } else if (n < strlen(input) - 1 && isspace(input[n]) && !isspace(input[n + 1])) {
+      *argumentCount = *argumentCount + 1;
+      
+    }
+  }
+    // printf("%d", *argumentCount);
 }
